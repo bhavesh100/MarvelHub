1d00ea14652e5c64fe7668f295dd7d3f66460fdd
Bhavesh Kumawat <kumawatbhavesh1000@gmail.com>
2023-06-16 14:01:38 +0530

    make some changes in existing code and

--- app/src/main/java/bhavesh/marvelhub/app/network/MarvelApi.kt
+++ app/src/main/java/bhavesh/marvelhub/app/network/MarvelApi.kt
@@ -1,24 +1,27 @@
 package bhavesh.marvelhub.app.network
 
 import bhavesh.marvelhub.app.network.model.charactersdto.CharactersDto
+import bhavesh.marvelhub.app.util.Constants
+import bhavesh.marvelhub.app.util.Constants.API_KEY
+import bhavesh.marvelhub.app.util.Constants.timeStamp
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MarvelApi {
 
-    @GET("/v1/public/characters")
+    @GET("characters")
     suspend fun getAllCharacters(
-        @Query("apikey")apikey:String,
-        @Query("ts")ts: String,
-        @Query("hash")hash: String,
+        @Query("apikey")apikey:String = API_KEY,
+        @Query("ts")ts: String = timeStamp,
+        @Query("hash")hash: String = Constants.hash(),
         @Query("offset")offset: String
     ) : CharactersDto
 
-    @GET("/v1/public/characters")
+    @GET("characters")
     suspend fun getAllSearchedCharacters(
-        @Query("apikey")apikey:String,
-        @Query("ts")ts: String,
-        @Query("hash")hash: String,
+        @Query("apikey")apikey:String = API_KEY,
+        @Query("ts")ts: String = timeStamp,
+        @Query("hash")hash: String = Constants.hash(),
         @Query("nameStartsWith")Search: String
     ) : CharactersDto
 
